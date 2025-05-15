a74d3530e86b560c0cb7d01ca5d0b55ffc6baad9
U Hrushikesh Karthik <143626493+Hrushikesh-karthik@users.noreply.github.com>
2025-05-15 22:39:40 +0530

    Update aws-instance-setup.sh

--- aws-instance-setup.sh
+++ aws-instance-setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Update all packages
-sudo yum update -y
+sudo apt update -y
 
 # Install Apache web server
-sudo yum install -y httpd
+sudo apt install -y apache2
 
 # Start the Apache service
-sudo systemctl start httpd
+sudo systemctl start apache2
+
+
 
 # Enable Apache to start on boot
-sudo systemctl enable httpd
+sudo systemctl enable apache2
 
 # Add a custom message to the default web page
 echo "<h1>Welcome to My Highly Available Web App - Instance 1</h1>" | sudo tee /var/www/html/index.html
+
